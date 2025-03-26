73fdc01a4f5112d859d469e111a4ec3f113f9b2c
kodenops <segun.femifadiya@gmail.com>
2025-03-26 14:38:32 +0100

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,47 @@
-import Image from "next/image";
+import React from 'react';
+import logo from '../../public/assets/logo.png';
+import bg from '../../public/assets/bg.png';
+import Image from 'next/image';
+import { BiLogoGmail } from 'react-icons/bi';
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+const page = () => {
+	return (
+		<div className='w-full h-screen flex md:items-center justify-center relative overflow-hidden'>
+			<div className='bg h-screen  w-full absolute'>
+				<Image
+					src={bg}
+					alt='logo image'
+					className='h-screen w-full object-cover'
+				/>
+			</div>
+			<div className='wrapper z-20 md:mt-0 mt-20'>
+				<div className='logo flex flex-col items-center gap-4'>
+					<Image
+						src={logo}
+						alt='logo image'
+						className='w-20'
+					/>
+					<p className='text-center text-white md:text-xl text-md'>
+						The simplify way to handle emails...Literally
+					</p>
+				</div>
+				<div className='loginForm py-8 md:px-20 px-4 bg-white rounded-sm mt-10 md:mx-0 mx-2'>
+					<div>
+						<h3 className='md:text-2xl text-xl font-semibold capitalize text-center text-[#07998C]'>
+							Sign In
+						</h3>
+						<p className='md:text-md text-md mt-2 text-center w-full text-[#7A7676] font-medium '>
+							You can only login with Google account currently
+						</p>
+					</div>
+					<button className='p-4 bg-[#07998C] w-full rounded-sm md:mt-10 mt-6 font-semibold text-white cursor-pointer flex items-center justify-center gap-4'>
+						<BiLogoGmail />
+						Proceed to login
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
-}
+export default page;
