4d3676d836fbf40d9d6cd6aef92010ddb449bfac
kodenops <segun.femifadiya@gmail.com>
2025-04-02 09:42:38 +0100

    updated the search field in inbox

--- src/app/inbox/page.tsx
+++ src/app/inbox/page.tsx
@@ -11,6 +11,7 @@ import { LiaUserClockSolid } from 'react-icons/lia';
 import { IoMdAlarm } from 'react-icons/io';
 import { HiOutlineBellAlert } from 'react-icons/hi2';
 import { IoSearchSharp } from 'react-icons/io5';
+import { RiFilter3Line } from 'react-icons/ri';
 
 const page = () => {
 	return (
@@ -68,15 +69,15 @@ const page = () => {
 							color='#E4E4E3'
 							Icon={HiOutlineBellAlert}
 						/>
-						<hr className='w-[94%] mx-[3%] border-1 border-[#c4c4c4]' />
+						<hr className='w-[94%] mx-[3%] border-1 border-[#c4c4c4] my-4' />
 						<div className='emailList w-full'>
 							<div className='header flex md:items-center justify-between w-full  md:flex-row flex-col'>
 								<h3 className='font-semibold text-md text-[#1D4642]'>
 									Unclassified Inbox{' '}
 									<span className='text-sm text-[#59514F]'>(234 mails)</span>
 								</h3>
-								<div className='search md:w-auto  w-full'>
-									<div className='search flex items-center  gap-4 border-1 px-4 py-2 md:w-md border-[#ebebeb] rounded-sm mt-4'>
+								<div className='search md:w-auto  w-full flex items-center justify-between  gap-3'>
+									<div className='search flex items-center  gap-4 border-1 px-4 py-2 md:w-md w-full mt-4 border-[#ebebeb] rounded-sm '>
 										<IoSearchSharp
 											size={25}
 											color='#c4c4c4'
@@ -87,6 +88,7 @@ const page = () => {
 											className='outline-none w-full placeholder:text-md border-transparent bg-transparent font-medium'
 										/>
 									</div>
+									<RiFilter3Line size={24} />
 								</div>
 							</div>
 						</div>
