5a1e6a5fe0b7b9c1d58d2651f1669db49f861907
kodenops <segun.femifadiya@gmail.com>
2025-04-02 09:31:42 +0100

    updated the inbox page

--- src/app/inbox/page.tsx
+++ src/app/inbox/page.tsx
@@ -10,6 +10,7 @@ import { LiaUserClockSolid } from 'react-icons/lia';
 
 import { IoMdAlarm } from 'react-icons/io';
 import { HiOutlineBellAlert } from 'react-icons/hi2';
+import { IoSearchSharp } from 'react-icons/io5';
 
 const page = () => {
 	return (
@@ -67,6 +68,28 @@ const page = () => {
 							color='#E4E4E3'
 							Icon={HiOutlineBellAlert}
 						/>
+						<hr className='w-[94%] mx-[3%] border-1 border-[#c4c4c4]' />
+						<div className='emailList w-full'>
+							<div className='header flex md:items-center justify-between w-full  md:flex-row flex-col'>
+								<h3 className='font-semibold text-md text-[#1D4642]'>
+									Unclassified Inbox{' '}
+									<span className='text-sm text-[#59514F]'>(234 mails)</span>
+								</h3>
+								<div className='search md:w-auto  w-full'>
+									<div className='search md:flex items-center  gap-4 border-1 px-4 py-2 md:w-md border-[#ebebeb] rounded-sm mt-4'>
+										<IoSearchSharp
+											size={25}
+											color='#c4c4c4'
+										/>
+										<input
+											type='text'
+											placeholder='Search mail'
+											className='outline-none w-full placeholder:text-md border-transparent bg-transparent font-medium'
+										/>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
